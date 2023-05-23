660bcda2a0e78d4373d988be61623eab8fbfff77
BT_3W2_02 <ronaldoc25@nycstudents.net>
2023-05-23 11:18:23 -0400

    f

--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Question {
+public class Question {
+
+    private String question;
+    private String answer;
+
+    public Question(String question, String answer){
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public boolean checkAnswer(String answer){
+        return this.answer.equals(answer);
+    }
 }
